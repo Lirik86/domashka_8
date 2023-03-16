018d60bf4b7753e98be62aba872ea7b6d8887bbe
Lirik <SobyaninKA@yandex.ru>
2023-03-16 15:17:54 +0500

    Выполнил 1 задание

---
+++ zadanie1/Program.cs
@@ -0,0 +1,66 @@
+﻿int input(string msg)
+{
+    System.Console.WriteLine($"{msg} >");
+    int value;
+    if(int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+int[,] myArray(int col, int row)
+{
+    int[,] array = new int[col, row];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(1,10);
+        }
+    }
+    return array;
+}
+
+int[,] requestArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1)-1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int col = input("Введите кол-во столбцов в матрице: ");
+int row = input("Введите кол-во строк в матрице: ");
+int[,] array = myArray(col, row);
+PrintArray(array);
+int[,] array2 = requestArray(array);
+PrintArray(array2);
