9be6ff901c9ccefecc92497cb520a5c41efb58f8
Lirik <SobyaninKA@yandex.ru>
2023-03-19 14:53:50 +0500

    выполнил 4 задание

---
+++ zadanie4/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] myArray(int num)
+{
+    int[,] array = new int[num, num];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = 0;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int[,] requstArray(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do {array[i, j++] = value++;} while (++k < n-1);
+        for (k = 0; k< n-1; k++) array[i++, j] = value++;
+        for (k = 0; k< n-1; k++) array[i, j--] = value++;
+        for (k = 0; k< n-1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0:n-2;
+    }
+    return array;
+}
+int num = 6;
+int [,] array = myArray(num);
+PrintArray(array);
+PrintArray(requstArray(array, num));
